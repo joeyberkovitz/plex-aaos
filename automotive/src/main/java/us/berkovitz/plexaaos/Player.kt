c22d302be65a8024d2101670794377c3f446bb77
Joey Berkovitz <17735957+joeyberkovitz@users.noreply.github.com>
2026-04-11 13:56:33 -0400

    cleanup

--- automotive/src/main/java/us/berkovitz/plexaaos/Player.kt
+++ automotive/src/main/java/us/berkovitz/plexaaos/Player.kt
@@ -2,7 +2,9 @@ package us.berkovitz.plexaaos
 
 import androidx.media3.common.ForwardingSimpleBasePlayer
 import androidx.media3.common.Player
+import androidx.media3.common.util.UnstableApi
 
+@UnstableApi
 class PlexPlayer(player: Player): ForwardingSimpleBasePlayer(player) {
 
 }
